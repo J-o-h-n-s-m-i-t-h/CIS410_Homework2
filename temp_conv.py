9813033f4caf97568d0f0f3e904c9bfdd2621ef2
J-o-h-n-s-m-i-t-h <143662733+J-o-h-n-s-m-i-t-h@users.noreply.github.com>
2023-10-09 23:23:28 -0600

    Update temp_conv.py with comments

--- temp_conv.py
+++ temp_conv.py
@@ -1,7 +1,8 @@
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return round(celsius)
-
+#  calculation from F to C
 def celsius_to_fahrenheit(celsius):
     fahrenheit = (celsius * 9/5) + 32
     return round(fahrenheit)
+    #  calculation from C to F
